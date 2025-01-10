1c33aa51290b64caace38439d337f205a3e627bf
Oscar Olsson <happybits@gmail.com>
2025-01-10 17:41:26 +0100

    -

--- MyWebApp/reset-database.sql
+++ MyWebApp/reset-database.sql
@@ -1,35 +1,35 @@
 
 -- Drop database if exists
-DROP DATABASE IF EXISTS mydbname;
+DROP DATABASE IF EXISTS product_database;
 
 -- Drop user if exists
-DROP USER IF EXISTS myuser;
+DROP USER IF EXISTS username;
 
 -- Create database
-CREATE DATABASE mydbname;
+CREATE DATABASE product_database;
 
 -- Create user with password
-CREATE USER myuser WITH PASSWORD 'mypassword';
+CREATE USER username WITH PASSWORD 'strong_password';
 
 -- Connect to the database
-\c mydbname
+\c product_database
 
 -- Grant all privileges on all tables (current and future)
-GRANT ALL PRIVILEGES ON DATABASE mydbname TO myuser;
-ALTER DATABASE mydbname OWNER TO myuser;
+GRANT ALL PRIVILEGES ON DATABASE product_database TO username;
+ALTER DATABASE product_database OWNER TO username;
 
 -- Create schema and grant permissions
 CREATE SCHEMA IF NOT EXISTS public;
-ALTER SCHEMA public OWNER TO myuser;
-GRANT ALL ON SCHEMA public TO myuser;
-GRANT ALL ON ALL TABLES IN SCHEMA public TO myuser;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO myuser;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO myuser;
+ALTER SCHEMA public OWNER TO username;
+GRANT ALL ON SCHEMA public TO username;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO username;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO username;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO username;
 
 -- Allow user to create new tables
-ALTER USER myuser CREATEDB;
+ALTER USER username CREATEDB;
 
 -- Set default privileges for future objects
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO myuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO myuser;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO myuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO username;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO username;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO username;
--- MyWebApp/tmp.ps1
+++
@@ -1,6 +0,0 @@
-docker run `
--p 1234:80 `
---network mywebapp-network `
-mywebapp 
-
-
